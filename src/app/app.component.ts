62291a99355c923b88c2808bc79a206e8cd30a1c
Chathuranga97lj <chathuit97@gmail.com>
2023-12-01 13:14:47 +0530

    add top-header as html-attribute

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AppHeaderComponent } from './app-header/app-header.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AppHeaderComponent],
+  imports: [CommonModule, RouterOutlet, AppHeaderComponent, TopHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
